7f7af0b5d93853e4a47145c25881ea587448de52
Zhannur <jannuraidynuly@gmail.com>
2022-01-01 18:37:54 +0500

    change dir

--- Greet.js
+++
@@ -1,8 +0,0 @@
-import mongoose from 'mongoose';
-
-const Greet = new mongoose.Schema({
-    title: {type: String, required: true},
-    picture: {type: String}
-})
-
-export default mongoose.model('Greet', Greet)
