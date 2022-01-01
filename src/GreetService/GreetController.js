3555824dfaf75077aa015b16edee8f979764bf53
Zhannur <jannuraidynuly@gmail.com>
2022-01-01 23:03:21 +0500

    add Access-Control-Allow-Origin: *

--- src/GreetService/GreetController.js
+++ src/GreetService/GreetController.js
@@ -2,45 +2,45 @@ import GreetServise from './GreetServise.js';
 
 class GreetController {
     async create(req, res) {
+        res.set('Access-Control-Allow-Origin', '*')
         try {
             const greet = await GreetServise.create(req.body, req.files.picture);
-            res.set('Access-Control-Allow-Origin', '*')
             res.status(200).json(greet)
         } catch (e) {
             res.status(500).json(e)
         }
     }
     async getAll(req, res) {
+        res.set('Access-Control-Allow-Origin', '*')
         try {
             const greets = await GreetServise.getAll();
-            res.set('Access-Control-Allow-Origin', '*')
             res.status(200).json(greets)
         } catch (e) {
             res.status(500).json(e)
         }
     }
     async getOne(req, res) {
+        res.set('Access-Control-Allow-Origin', '*')
         try {
             const greet = await GreetServise.getOne(req.params.id)
-            res.set('Access-Control-Allow-Origin', '*')
             res.status(200).json(greet)
         } catch (e) {
             res.status(500).json(e)
         }
     }
     async update(req, res) {
+        res.set('Access-Control-Allow-Origin', '*')
         try {
             const updateGreet = await GreetServise.update(req.body)
-            res.set('Access-Control-Allow-Origin', '*')
             res.status(200).json(updateGreet)
         } catch (e) {
             res.status(500).json(e)
         }
     }
     async delete(req, res) {
+        res.set('Access-Control-Allow-Origin', '*')
         try {
             const greet = await GreetServise.delete(req.params.id)
-            res.set('Access-Control-Allow-Origin', '*')
             res.status(200).json(greet)
         } catch (e) {
             res.status(500).json(e)
